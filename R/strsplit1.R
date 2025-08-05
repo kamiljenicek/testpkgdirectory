d75c527708e0e34c5019851130bfd50b2176c68e
kamiljenicek <Kamil.Jenicek@email.com>
2025-08-05 13:38:34 +0200

    change the code of the file

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -9,7 +9,15 @@
 #' @examples
 #' x <- "a,b,c,d"
 #' strsplit1(x,",")
-strsplit1 <- function(x, split){
+str_split_one <- function(string, pattern, n = Inf){
+
+  stopifnot(is.character(string),length(string) <= 1)
+
+  if (length(string)==1) {
+    stringr::str_split(string = string, pattern = pattern, n=n)[[1]]
+  } else{
+    character()
+  }
 
 strsplit(x, split = split)[[1]]
 
